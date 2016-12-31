563ce73fb0dd84b6673e030c51e8c4598f4a2e55
진형탁 <njir@naver.com>
2016-12-31 15:13:47 +0900

    feat: First commit for pitch app

---
+++ intro-to-ios-app-development-with-swift.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// variables
+var x: Int = 42
+let y = 100 //constant
+
+var myString = "Hello"
+
+// control flow
+if x < 50 {
+    print("X is less than 50")
+} else {
+    print("X is greater than or equal to 50")
+}
+
+
+// classes
+class ViewController: UIViewController {
+    // instance var go here
+    // class function go here
+}
+
+// functions
+func printHello() {
+    print("Hello")
+}
+
+printHello()
+
+func printHelloMessage(helloString: String){
+    print(helloString)
+}
+
+
+
