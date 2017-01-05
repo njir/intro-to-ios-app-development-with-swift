cc70897ac44557665a4e99d3e1a13829290fade0
진형탁 <njir@naver.com>
2017-01-06 00:02:18 +0900

    feat: Add homework by tutor

--- intro-to-ios-app-development-with-swift.playground/Contents.swift
+++ intro-to-ios-app-development-with-swift.playground/Contents.swift
@@ -35,5 +35,16 @@ func printHelloMessage(helloString: String){
     print(helloString)
 }
 
+printHelloMessage(helloString: "test")
+
+func join(s1: String, s2: String, joiner: String) -> String {
+    return s1 + joiner + s2
+}
+
+join(
+
+printHelloMessage(helloString: join(s1: "foo", s2: "bar", joiner: ", "))
+
+
 
 
